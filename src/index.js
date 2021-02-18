8e5f73763a01469d0e6f912db87afbd08bb5e50b
Luke Thinnes <lukethinnes@yahoo.com>
2021-02-18 15:18:13 -0700

    Updated design.

--- src/index.js
+++ src/index.js
@@ -4,43 +4,43 @@ baseURL = 'http://localhost:3000'
 const albumForm = document.querySelector('.add-album')
 const albumCollection = document.querySelector('#albums')
 
-// albumForm.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     console.log(event)
-//     const formData = new FormData(event.target)
-//     const albumTitle = formData.get('title')
-//     const albumCover = formData.get('cover')
-//     const albumYear = formData.get('year')
+albumForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log(event)
+    const formData = new FormData(event.target)
+    const albumTitle = formData.get('title')
+    const albumCover = formData.get('cover')
+    const albumYear = formData.get('year')
 
-//     const album = {
-//         title: albumTitle,
-//         cover: albumCover,
-//         year: albumYear
-//     }
+    const album = {
+        title: albumTitle,
+        cover: albumCover,
+        year: albumYear
+    }
 
-//     const albumCard = document.createElement('div')
-//     const title = document.createElement('h4')
-//     const image = document.createElement('img')
-//     const year = document.createElement('p')
-//     const deleteButton = document.createElement('button')
+    const albumCard = document.createElement('div')
+    const title = document.createElement('h4')
+    const image = document.createElement('img')
+    const year = document.createElement('p')
+    const deleteButton = document.createElement('button')
 
-//     albumCard.className = 'card'
-//     title.textContent = albumTitle
-//     image.src = image
-//     image.className = 'cover'
-//     year.textContent = albumYear
-//     deleteButton.className = 'delete'
-//     deleteButton.textContent = 'Remove'
+    albumCard.className = 'card'
+    title.textContent = albumTitle
+    image.src = image
+    image.className = 'cover'
+    year.textContent = albumYear
+    deleteButton.className = 'delete'
+    deleteButton.textContent = 'Remove'
 
-//     albumCard.append(title, image, year, deleteButton)
-//     albumCollection.appendChild(albumCard)
+    albumCard.append(title, image, year, deleteButton)
+    albumCollection.appendChild(albumCard)
 
-//     fetch(baseURL + '/albums', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(album)
-//     })
-// })
+    fetch(baseURL + '/albums', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(album)
+    }).then(() => window.location.replace('/'))
+})
 
 fetch(baseURL + '/albums')
     .then(response => response.json())
